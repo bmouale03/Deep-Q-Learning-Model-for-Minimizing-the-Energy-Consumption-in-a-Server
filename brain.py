2b1884834c5b1b2c96da1e643fb8372ee47d4807
bmouale03 <bmouale@mail.ru>
2025-09-26 18:53:46 +0300

    first commit

---
+++ brain.py
@@ -0,0 +1,28 @@
+#Mise en œuvre d’un algorithme de Deep Q-Learning intégrant
+#le modèle de régression linéaire multiple pour l’optimisation 2nergétique des Data Center
+# Building the Brain# Developpement du cerveau
+
+# Importing the libraries# Import des librairies necessaires
+from keras.layers import Input, Dense, Dropout
+from keras.models import Model
+from keras.optimizers import Adam # un optimizeur
+
+# BUILDING THE BRAIN# CREATION DU CERVEAU
+class Brain(object):
+    def __init__(self, learning_rate=0.001, number_actions=5):
+        self.learning_rate = learning_rate
+        # BUILDIND THE INPUT LAYER COMPOSED OF THE INPUT STATE# CREATION DE LA  COUCHE D'ENTREE
+        states = Input(shape = (3,))
+        # BUILDING THE FULLY CONNECTED HIDDEN LAYERS# LES COUCHES CACHEES
+        x = Dense(units = 64, activation = 'sigmoid')(states) 
+        x = Dropout(rate = 0.1)(x)
+        y = Dense(units = 32, activation = 'sigmoid')(x)
+        y = Dropout(rate = 0.1)(y)
+        # BUILDING THE OUTPUT LAYER, FULLY CONNECTED TO THE LAST HIDDEN LAYER# CREATION DE LA  COUCHE DE SORTIE
+        q_values = Dense(units = number_actions, activation = 'softmax')(y)# Sofmax est beaucoup plus utiliser pour la classification car elle permet d'obtenir les probabilités
+        # ASSEMBLING THE FULL ARCHITECTURE INSIDE A MODEL OBJECT#
+        self.model = Model(inputs = states, outputs = q_values)# CREATION DU MODELE
+        # COMPILING THE MODEL WITH A MEAN-SQUARED ERROR LOSS AND A CHOSEN OPTIMIZER# COMPILATION DU MODELE
+        self.model.compile(loss = 'mse', optimizer = Adam(lr = learning_rate))# mse pour les problemes de regressions
+        
+        
---
+++ dqn.py
@@ -0,0 +1,41 @@
+#Mise en œuvre d’un algorithme de Deep Q-Learning intégrant
+#le modèle de régression linéaire multiple pour l’optimisation 2nergétique des Data Center
+# Implementing Deep Q-Learning with Experience Replay# Implementtion de l'algo deep q learning
+
+# Importing the libraries# Import des libraries
+import numpy as np
+
+class DQN(object):
+    # INTRODUCING AND INITIALIZING ALL THE PARAMETERS AND VARIABLES OF THE DQN# INITIALISATION DE LA CLASSE
+    def __init__(self, max_memory=100, discount=0.9):
+        self.memory = list() 
+        self.max_memory = max_memory 
+        self.discount = discount
+    # MAKING A METHOD THAT BUILDS THE MEMORY IN EXPERIENCE REPLAY# METHODE POUR LE PRECESSUS DE REMPLISSAGE DE L'EXPERIENCE REPLAY
+    def remember(self, transition, game_over):
+        self.memory.append([transition, game_over])
+        if len(self.memory) > self.max_memory:
+            del self.memory[0]
+    # MAKING A METHOD THAT BUILDS TWO BATCHES OF INPUTS AND TARGE# CREATION OU OBTENTION DE DEUX BACHES(ENTRE ET LA CIBLE)
+    def get_batch(self, model, batch_size = 10):
+        len_memory = len(self.memory) 
+        num_inputs = self.memory[0][0][0].shape[1] 
+        num_outputs = model.output_shape[-1] 
+        inputs = np.zeros((min(len_memory, batch_size), num_inputs)) 
+        targets = np.zeros((min(len_memory, batch_size), num_outputs))
+        transitions= np.random.randint(0, len_memory,size = min(len_memory, batch_size))
+        for i, idx in enumerate(transitions):
+            current_state, action, reward, next_state = self.memory[idx][0]
+            #current_state, action, reward, next_state = self.memory[idx][0]
+            game_over = self.memory[idx][1]
+            inputs[i] = current_state
+            targets[i] = model.predict(current_state)[0]
+            #Q_sa = np.max(model.predict(next_state)[0])
+            if game_over==1:
+                targets[i, action] = reward
+            else:
+                targets[i, action] = reward + self.discount *np.max(model.predict(next_state)[0])
+        return inputs, targets
+
+
+
